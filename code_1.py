117bc1d8bd65711354fbe7f5779451c29ea1231a
evgenwebmaster23 <161200325+evgenwebmaster23@users.noreply.github.com>
2024-08-21 12:52:39 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,14 +1,14 @@
+def generate_random_data():
     data = [random.randint(1, 100) for _ in range(10)]
     main()
-def generate_random_data():
+
     for item in data:
+
+
 def main():
-
-    data = generate_random_data()
-
-
-        print(f"Random Number: {item}")
 if __name__ == "__main__":
+    data = generate_random_data()
+        print(f"Random Number: {item}")
 
     return data
 import random
